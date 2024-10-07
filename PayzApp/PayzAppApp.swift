f0b98eec9e39049d3e28f65d6230e9e23dd9e002
Nagaraju0323 <nagarajukios85@gmail.com>
2024-10-08 01:05:07 +0530

    Initial Commit

---
+++ PayzApp/PayzAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayzAppApp.swift
+//  PayzApp
+//
+//  Created by Nagaraju on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PayzAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
